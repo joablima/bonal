597ad8cd367ecf5d43e2bbd34c9e4a316b31191a
ssval <ssval@192.168.1.5>
2018-01-08 12:30:56 -0500

    só pra constar...

--- bonal/src/main/java/br/com/empresa/bonal/controles/TemaControle.java
+++ bonal/src/main/java/br/com/empresa/bonal/controles/TemaControle.java
@@ -1,11 +1,14 @@
 package br.com.empresa.bonal.controles;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class TemaControle {
+public class TemaControle implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	private String tema = "aristo";
 
